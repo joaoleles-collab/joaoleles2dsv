e4f08269460b8ff7ddf6a7c591e08a1e06e79f87
joaoleles-collab <joaop.leles26@gmail.com>
2026-04-16 17:27:54 -0300

    AULA 1 JS- ATIVIDADES
    ATIVIDADES

---
+++ Javascript/AULA 1- ATIVIDADE/scripts/atividade.js
@@ -0,0 +1,6 @@
+var valor = prompt("Valor da conta: ");
+var quantidade = prompt ("Quantas pessoas na mesa? ");
+
+var valor_total = valor/quantidade
+
+console.log(`O valor dividido para cada um é: R$ ${valor_total}`)
---
+++ Javascript/AULA 1- ATIVIDADE/scripts/atividade2.js
@@ -0,0 +1,6 @@
+var numero1 = prompt("Qual o número?");
+var antecessor= numero1-1;
+var sucessor= parseInt(numero1)+ 1;
+
+console.log(`A sequência numérica é: ${antecessor}, ${numero1}, ${sucessor}`);
+
