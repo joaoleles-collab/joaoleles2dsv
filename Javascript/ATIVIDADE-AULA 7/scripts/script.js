306e48fafcf343b304a4dc7c415da8db32fc03f0
joaoleles-collab <joaop.leles26@gmail.com>
2026-06-09 18:14:36 -0300

    ATIVIDADE-AULA 7
    AULA 7

---
+++ Javascript/ATIVIDADE-AULA 7/scripts/script.js
@@ -0,0 +1,16 @@
+var Nome = "João"
+let Idade = 16
+let online = true
+  
+
+
+let jogoFavorito = {
+    nome: "PES 2021",
+    Lancamento: 2020  
+}
+
+let pontuacoes = [2-0, 2-1, 3-0]
+
+console.log(Nome, Idade, online, jogoFavorito,);
+console.log("Ultimas pontuações nos jogos " + pontuacoes)
+
