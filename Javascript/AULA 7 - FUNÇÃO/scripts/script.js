3072e56438552f6cba18d09413a23e70b2591d34
joaoleles-collab <joaop.leles26@gmail.com>
2026-06-09 16:56:22 -0300

    AULA 7 - JS
    FUNÇÕES

---
+++ Javascript/AULA 7 - FUNÇÃO/scripts/script.js
@@ -0,0 +1,25 @@
+// SEM return - calcular mas perde o valor
+function calcularSem(preco, desconto) {
+    var total = preco - (preco * desconto/100);
+}
+
+function calcularCom(preco, desconto) {
+    var total = preco - (preco * desconto/100);
+    return total;
+}
+
+function testarSemReturn() {
+    var resultado = calcularSem(100, 50);
+    document.getElementById('resultado').innerHTML =
+    '<div class="alert alert-danger">'+
+    'Resultado: ' + resultado +
+    '(undefined - a função não evolveu nada)</div>';
+}
+
+function testarComReturn() {
+    var resultado = calcularCom(100, 50);
+    document.getElementById('resultado').innerHTML =
+    '<div class="alert alert-sucess">' +
+    'Resultado: R$' + resultado +
+    '(a função devolveu 50!</div>';
+}
